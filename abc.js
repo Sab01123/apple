5ff25093cda3cd6c5d361e70eec331c553a21de2
Sab01123 <104069112+Sab01123@users.noreply.github.com>
2022-04-28 21:10:26 +0530

    hello

---
+++ abc.js
@@ -0,0 +1,31 @@
+// for(let i=0; i<10; i++){
+
+// }
+// console.log(i)
+
+// let n =20
+
+// var myArray = [ null, 1, 5, undefined]
+
+// myArray.reduce(function(el){
+//   return el 
+// })
+
+// console.log(el)
+
+// function sum(){
+//     let a=b=10;
+//     console.log(a)
+//     return a
+// }
+// sum()
+// console.log(a)
+// console.log(b)
+
+function sum(num1,num2){
+    return num1+num2
+}
+function num(){
+    return 5
+}
+sum(num(),4)
